ed864f5c44c7abbf21c2c99e2742595ea7175c40
ing-thejis <ing.jesithgarcia@gmail.com>
2022-01-19 11:44:12 -0500

    recordar la ultima pagina visitada - usando useLocation del react-router-dom y localStorage

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,10 +1,14 @@
 import { useContext } from 'react'
 import { AuthContext } from '../auth/authContext'
-import { Navigate } from 'react-router-dom'
+import { Navigate, useLocation } from 'react-router-dom'
 
 const PrivateRoute = ({ children }) => {
 
 	const { user } = useContext(AuthContext)
+
+	const { pathname, search } = useLocation()
+
+	localStorage.setItem('lastPath', pathname + search)
 
 	return user.logged
 		? children
